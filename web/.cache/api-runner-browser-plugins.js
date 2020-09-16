273a6edaa44506dba9d5b412be928b18a24f6def
Efe Kurnaz <10762792+efekurnaz@users.noreply.github.com>
2020-09-16 13:33:28 +0300

    remove font loader plugin

--- web/.cache/api-runner-browser-plugins.js
+++ web/.cache/api-runner-browser-plugins.js
@@ -1,4 +1,7 @@
 module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-web-font-loader/gatsby-browser.js'),
+      options: {"plugins":[],"custom":{"families":["Rosalindale"],"urls":["/fonts/fonts.css"]}},
+    },{
       plugin: require('../gatsby-browser.js'),
       options: {"plugins":[]},
     }]
