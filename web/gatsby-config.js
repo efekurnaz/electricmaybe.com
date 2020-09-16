78cae3506fd671eabf0fb3dc491849b7d4a92cf2
Efe Kurnaz <10762792+efekurnaz@users.noreply.github.com>
2020-09-16 18:31:16 +0300

    fix typo in gatsby config

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-google-analytics",
       options: {
-        trackingId: provess.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
       }
     }
   ]
