4e30ac96b9d4c3fc99abcd49772970f159636c6d
efekurnaz <efekurnaz@gmail.com>
2021-07-11 17:30:13 +0300

    feat :rocket: superfat added

--- web/.cache/api-runner-browser-plugins.js
+++ web/.cache/api-runner-browser-plugins.js
@@ -2,6 +2,9 @@ module.exports = [{
       plugin: require('../node_modules/gatsby-plugin-web-font-loader/gatsby-browser.js'),
       options: {"plugins":[],"custom":{"families":["Rosalindale"],"urls":["/fonts/fonts.css"]}},
     },{
+      plugin: require('../node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
+      options: {"plugins":[],"trackingId":"none"},
+    },{
       plugin: require('../gatsby-browser.js'),
       options: {"plugins":[]},
     }]
--- web/.cache/sync-requires.js
+++ web/.cache/sync-requires.js
@@ -5,13 +5,13 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---src-templates-project-js": hot(preferDefault(require("/Users/efecankurnaz/Projects/electricmaybe.com/web/src/templates/project.js"))),
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/efecankurnaz/Projects/electricmaybe.com/web/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/efecankurnaz/Projects/electricmaybe.com/web/src/pages/404.js"))),
-  "component---src-pages-about-js": hot(preferDefault(require("/Users/efecankurnaz/Projects/electricmaybe.com/web/src/pages/about.js"))),
-  "component---src-pages-blog-js": hot(preferDefault(require("/Users/efecankurnaz/Projects/electricmaybe.com/web/src/pages/blog.js"))),
-  "component---src-pages-contact-js": hot(preferDefault(require("/Users/efecankurnaz/Projects/electricmaybe.com/web/src/pages/contact.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/efecankurnaz/Projects/electricmaybe.com/web/src/pages/index.js"))),
-  "component---src-pages-projects-js": hot(preferDefault(require("/Users/efecankurnaz/Projects/electricmaybe.com/web/src/pages/projects.js")))
+  "component---src-templates-project-js": hot(preferDefault(require("/Users/efecankurnaz/Documents/Projects/electricmaybe.com/web/src/templates/project.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/efecankurnaz/Documents/Projects/electricmaybe.com/web/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/efecankurnaz/Documents/Projects/electricmaybe.com/web/src/pages/404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/Users/efecankurnaz/Documents/Projects/electricmaybe.com/web/src/pages/about.js"))),
+  "component---src-pages-blog-js": hot(preferDefault(require("/Users/efecankurnaz/Documents/Projects/electricmaybe.com/web/src/pages/blog.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("/Users/efecankurnaz/Documents/Projects/electricmaybe.com/web/src/pages/contact.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/efecankurnaz/Documents/Projects/electricmaybe.com/web/src/pages/index.js"))),
+  "component---src-pages-projects-js": hot(preferDefault(require("/Users/efecankurnaz/Documents/Projects/electricmaybe.com/web/src/pages/projects.js")))
 }
 
